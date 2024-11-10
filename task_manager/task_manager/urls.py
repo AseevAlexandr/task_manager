02b55a51362bf71209501e2a5a5b4db696d7e822
alexandr_fin_calc <aleksandraseev14212@gmail.com>
2024-11-10 03:03:37 -0800

    + token

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -17,10 +17,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Task Manager API",
+      default_version='v1',
+      description="API для управления пользователями и задачами",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@taskmanager.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/', schema_view.as_view()),
+
 ]
